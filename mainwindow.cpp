acfccb4e73b6f0572fca3be906bc7438d69a231d
Bridget <erbb@spu.edu>
2020-01-27 19:38:16 -0800

    UI mostly done for pg 1

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,9 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->dateEdit->setDate(QDate::currentDate());
+    calculateRoom();
+
 }
 
 MainWindow::~MainWindow()
@@ -13,6 +16,29 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+void MainWindow::calculateRoom()
+{
+    double subTotal = 0;
+    if (ui->StandardQueen->isChecked()){
+        subTotal = 284;
+        ui->adultSpinBox->setMaximum(4);
+    }
+    if (ui->StandardKing->isChecked()){
+        subTotal = 290;
+        ui->adultSpinBox->setMaximum(3);
+    }
+    if (ui->AtriumQueen->isChecked()){
+        subTotal = 325;
+        ui->adultSpinBox->setMaximum(4);
+    }
+    if (ui->AtriumKing->isChecked()){
+        subTotal = 350;
+        ui->adultSpinBox->setMaximum(3);
+    }
+    subTotal *= ui->numberOfDaysSpinBox->value();
+    if (ui->vehicleParkingCheckBox->isChecked()){
+        subTotal += (12.75 * ui->numberOfDaysSpinBox->value());
+    }
 
 void MainWindow::on_nextPageButton_clicked()
 {
